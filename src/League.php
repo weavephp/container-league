f768fd4793074767bd8f007c72052f2e73ca171a
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2017-11-05 03:14:10 +0000

    Coding standards and Phan fixes.

--- src/League.php
+++ src/League.php
@@ -6,71 +6,86 @@ declare(strict_types = 1);
 namespace Weave\Container\League;
 
 use League\Container\Container;
+
 /**
  * Weave League DIC Adaptor.
  */
 trait League
 {
+	use \Weave\Container\Container;
+
 	/**
 	 * The Container instance.
 	 *
 	 * @var Container
 	 */
-	protected $_container;
+	protected $container;
 
 	/**
 	 * Setup the Dependency Injection Container
 	 *
-	 * @param array  $config      Optional config array as provided from _loadConfig().
+	 * @param array  $config      Optional config array as provided from loadConfig().
 	 * @param string $environment Optional indication of the runtime environment.
 	 *
 	 * @return callable A callable that can instantiate instances of classes from the DIC.
 	 */
-	protected function _loadContainer(array $config = [], $environment = null)
+	protected function loadContainer(array $config = [], $environment = null)
 	{
-		$this->_container = new Container;
-		$this->_configureContainerInternal();
-		$this->_configureContainer($this->_container, $config, $environment);
-		return $this->_container->get('instantiator');
+		$this->container = new Container;
+		$this->configureContainerInternal();
+		$this->configureContainer($this->container, $config, $environment);
+		return $this->container->get('instantiator');
 	}
 
 	/**
 	 * Configures the container.
 	 *
 	 * @param Container $container   The container.
-	 * @param array     $config      Optional config array as provided from _loadConfig().
+	 * @param array     $config      Optional config array as provided from loadConfig().
 	 * @param string    $environment Optional indication of the runtime environment.
 	 *
 	 * @return null
 	 */
-	abstract protected function _configureContainer(Container $container, array $config = [], $environment = null);
+	abstract protected function configureContainer(Container $container, array $config = [], $environment = null);
 
 	/**
 	 * Configure's the internal Weave requirements.
 	 *
 	 * @return null
 	 */
-	protected function _configureContainerInternal()
+	protected function configureContainerInternal()
 	{
-		$this->_container->add(
+		$this->container->add(
 			'instantiator',
-			function () { return function ($name) { return $this->_container->get($name); }; }
+			function () {
+				return function ($name) {
+					return $this->container->get($name);
+				};
+			}
 		);
 
-		$this->_container->add(\Weave\Middleware\Middleware::class)
+		$this->container->add(\Weave\Middleware\Middleware::class)
 		->withArgument(\Weave\Middleware\MiddlewareAdaptorInterface::class)
-		->withArgument(function ($pipelineName) { return $this->_provideMiddlewarePipeline($pipelineName); })
+		->withArgument(
+			function ($pipelineName) {
+				return $this->provideMiddlewarePipeline($pipelineName);
+			}
+		)
 		->withArgument('instantiator')
 		->withArgument(\Weave\Http\RequestFactoryInterface::class)
 		->withArgument(\Weave\Http\ResponseFactoryInterface::class)
 		->withArgument(\Weave\Http\ResponseEmitterInterface::class);
 
-		$this->_container->add(\Weave\Middleware\Dispatch::class)
+		$this->container->add(\Weave\Middleware\Dispatch::class)
 		->withArgument('instantiator');
 
-		$this->_container->add(\Weave\Router\Router::class)
+		$this->container->add(\Weave\Router\Router::class)
 		->withArgument(\Weave\Router\RouterAdaptorInterface::class)
-		->withArgument(function ($router) { return $this->_provideRouteConfiguration($router); })
+		->withArgument(
+			function ($router) {
+				return $this->provideRouteConfiguration($router);
+			}
+		)
 		->withArgument('instantiator');
 	}
 }
